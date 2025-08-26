1d5e4aa4787b67bde56afd7c1c04cf9393020a89
Kalana-indula <kalanasmedia@outlook.com>
2025-08-26 12:27:29 +0530

    add event page added, booking page added

--- app/(control)/manager/(control-panel)/dashboard/page.tsx
+++ app/(control)/manager/(control-panel)/dashboard/page.tsx
@@ -138,8 +138,6 @@ const Page = () => {
             }
         }
     };
-
-
     return (
         <>
             {/*Header section*/}
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+//handle exceptions
+// utils/errorHandler.ts
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+export function handleApiError(err: unknown, fallbackMessage = "Something went wrong") {
+  if (axios.isAxiosError(err)) {
+    toast.error(
+        err.response?.data?.message ||
+        err.response?.statusText ||
+        fallbackMessage
+    );
+  } else if (err instanceof Error) {
+    toast.error(err.message);
+  } else {
+    toast.error(fallbackMessage);
+  }
+}
